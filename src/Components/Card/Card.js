7d2e20a2112f06e48e29dc072117e24e6df59ac2
Edward Stoner <edwardlstoner@gmail.com>
2020-04-02 11:25:31 -0600

    Pass down delete function

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Card.css";
 
-const Card = ({ name, date, number, time, id }) => {
+const Card = ({ name, date, number, time, id, deleteReservation }) => {
   return (
     <div className="card">
       <div className="reservation-name reservation-category">{name}</div>
       <div className="reservation-category">{date}</div>
       <div className="reservation-category">{time}</div>
       <div className="reservation-category">Number of guests: {number}</div>
-      <button className="cancel-btn">Cancel</button>
+      <button className="cancel-btn" onClick={() => deleteReservation(id)}>
+        Cancel
+      </button>
     </div>
   );
 };
