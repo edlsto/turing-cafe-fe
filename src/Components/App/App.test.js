e373854ff9d64df4fa926d92136ebd55c610e982
Edward Stoner <edwardlstoner@gmail.com>
2020-04-02 11:48:29 -0600

    Add mocked values

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,22 +1,58 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App";
+import { getData, postNewReservation } from "../../api-calls.js";
+jest.mock("../../api-calls.js");
 
-it("displays a new reservation when a user submits a reservation", () => {
-  const { getByPlaceholderText, getByText, debug } = render(<App />);
+it("displays a new reservation when a user submits a reservation", async () => {
+  postNewReservation.mockResolvedValueOnce({
+    id: 18939837,
+    name: "new test user",
+    date: "4/11",
+    time: "8:30",
+    number: "100"
+  });
+  getData.mockResolvedValueOnce([
+    {
+      id: 1,
+      name: "Christie",
+      date: "12/29",
+      time: "7:00",
+      number: 12
+    },
+    {
+      id: 2,
+      name: "Leta",
+      date: "4/5",
+      time: "7:00",
+      number: 2
+    },
+    {
+      id: 3,
+      name: "Pam",
+      date: "1/21",
+      time: "6:00",
+      number: 4
+    }
+  ]);
+  const { getByPlaceholderText, getByText } = render(<App />);
   const nameInput = getByPlaceholderText("Name");
   const dateInput = getByPlaceholderText("Date (mm/dd)");
   const timeInput = getByPlaceholderText("Time");
   const numberInput = getByPlaceholderText("Number of guests");
   const submitBtn = getByText("Make Reservation");
-  fireEvent.change(nameInput, { target: { value: "Ed" } });
+  fireEvent.change(nameInput, { target: { value: "new test user" } });
   fireEvent.change(dateInput, { target: { value: "4/11" } });
   fireEvent.change(timeInput, { target: { value: "8:30" } });
-  fireEvent.change(numberInput, { target: { value: "4" } });
+  fireEvent.change(numberInput, { target: { value: "100" } });
   fireEvent.click(submitBtn);
-  expect(getByText("Ed")).toBeInTheDocument();
-  expect(getByText("4/11")).toBeInTheDocument();
-  expect(getByText("8:30")).toBeInTheDocument();
-  expect(getByText("Number of guests: 4")).toBeInTheDocument();
+  const nameText = await waitFor(() => getByText("new test user"));
+  const dateText = await waitFor(() => getByText("4/11"));
+  const timeText = await waitFor(() => getByText("8:30"));
+  const numberText = await waitFor(() => getByText("Number of guests: 100"));
+  expect(nameText).toBeInTheDocument();
+  expect(dateText).toBeInTheDocument();
+  expect(timeText).toBeInTheDocument();
+  expect(numberText).toBeInTheDocument();
 });
