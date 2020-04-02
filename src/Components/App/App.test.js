c2203ae1bbeecc56d0b901f9e3ab532eef2e6829
Edward Stoner <edwardlstoner@gmail.com>
2020-04-02 11:01:18 -0600

    Write integration test for App.js

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("displays a new reservation when a user submits a reservation", () => {
+  const { getByPlaceholderText, getByText, debug } = render(<App />);
+  const nameInput = getByPlaceholderText("Name");
+  const dateInput = getByPlaceholderText("Date (mm/dd)");
+  const timeInput = getByPlaceholderText("Time");
+  const numberInput = getByPlaceholderText("Number of guests");
+  const submitBtn = getByText("Make Reservation");
+  fireEvent.change(nameInput, { target: { value: "Ed" } });
+  fireEvent.change(dateInput, { target: { value: "4/11" } });
+  fireEvent.change(timeInput, { target: { value: "8:30" } });
+  fireEvent.change(numberInput, { target: { value: "4" } });
+  fireEvent.click(submitBtn);
+  expect(getByText("Ed")).toBeInTheDocument();
+  expect(getByText("4/11")).toBeInTheDocument();
+  expect(getByText("8:30")).toBeInTheDocument();
+  expect(getByText("Number of guests: 4")).toBeInTheDocument();
 });
