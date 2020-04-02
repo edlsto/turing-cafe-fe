fb1d9f5a0bd8d4645288172d181cca5387aa47bc
Edward Stoner <edwardlstoner@gmail.com>
2020-04-02 10:35:17 -0600

    Add add reservation functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import CardsContainer from "../CardsContainer/CardsContainer";
+import Form from "../Form/Form";
 
 class App extends Component {
   constructor(props) {
@@ -14,11 +15,16 @@ class App extends Component {
       .then(response => response.json())
       .then(data => this.setState({ reservations: data }));
   }
+
+  addReservation = reservation => {
+    this.setState({ reservations: [reservation, ...this.state.reservations] });
+  };
+
   render() {
     return (
       <div className="App">
         <h1 className="app-title">Turing Cafe Reservations</h1>
-        <div className="resy-form"></div>
+        <Form className="resy-form" addReservation={this.addReservation} />
         <CardsContainer
           className="resy-container"
           reservations={this.state.reservations}
